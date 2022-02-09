40634a4bf117db1713e5e0afcc1c636eac77ef23
Icaro de Barros <icarodebarros@gmail.com>
2022-02-09 12:24:20 -0300

    Creating comps MeetupList and MeetupItem

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from "../components/meetups/MeetupList";
+
 const DUMMY_DATA = [
   {
     id: "m1",
@@ -22,10 +24,8 @@ const DUMMY_DATA = [
 function AllMeetupsPage() {
   return (
     <section>
-      <div>All Meetups</div>
-      {DUMMY_DATA.map((meetup) => {
-        return <li key={meetup.id}>{meetup.title}</li>;
-      })}
+      <h1>All Meetups</h1>
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
