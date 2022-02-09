e80c7fa27b261be211341ab0d956cf96d287ba20
Icaro de Barros <icarodebarros@gmail.com>
2022-02-09 16:15:28 -0300

    Using the useEffect hook

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,47 @@
-import MeetupList from "../components/meetups/MeetupList";
+import { useState, useEffect } from "react";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import MeetupList from "../components/meetups/MeetupList";
+import ENV from '../environment/environment';
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`${ENV.URL_BASE}/meetups.json`) // get by default
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const meetups = [];
+
+        for (const key in data) { // changing the firebase default format
+          const meetup = { id: key, ...data[key] };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []); /* List of dependencies that if changed will cause the above function
+  to execute. An empty array will execute the function only when the component 
+  render the first time. Ommit the array is the same of having our function outside
+  the useEffect. 
+  The golden rule is list the outsiders dependencies. (In our case we dont have any) */
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
